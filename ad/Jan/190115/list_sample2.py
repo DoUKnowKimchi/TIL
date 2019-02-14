e8fa0d0ef1b9b8db18e00637a9620765e23e4f57
DoYouKnowKimchi <kaei2574@gmail.com>
2019-02-14 13:17:46 +0900

    hi

--- ad/Jan/190115/list_sample2.py
+++ ad/Jan/190115/list_sample2.py
@@ -1,18 +1,18 @@
 T = int(input())
 for t in range(1, T+1):
-    n = 0
-    r = 0
-    i = 0
     K, N, M = list(map(int, input().split()))
     L = list(map(int, input().split()))
-    while not n==N:
-        n += K
-        while True:
-            if n in L:
-                r += 1
-                break
-            else:
-                n -= 1    
-        # 최소 충전회수 구하기
-        # 고려할 사항 : K거리에 정류장이 여러개이면?
+    r = 0
+    n = 0
+    while n<N:
+        n_b = list(filter(lambda x:x < n+K, L))
+        if n >= N - K:
+            break
+        elif len(n_b) == 0:
+            r = 0
+            break
+        else :
+            r += 1
+            n = n_b[-1]
+            L = L[L.index(n)+1:] 
     print(f"#{t} {r}")
